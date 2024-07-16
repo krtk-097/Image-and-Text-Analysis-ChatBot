c9ee2ed8d4a91a90d03ef703fbb5beb51132cde5
Kartik <frankyz2190@gmail.com>
2024-07-16 19:33:10 +0530

    cors error fix

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,13 @@ const port = process.env.PORT || 5000;
 
 connectDB();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://image-and-text-analysis-chat-bot-server.vercel.app/"],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use("/api", apiRoutes);
 
